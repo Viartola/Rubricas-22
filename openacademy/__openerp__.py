e8f5bf57727397c599d6e2740d0da29bc37b9c2b
Jaime <jaime.viartola@gmail.com>
2017-01-14 17:08:41 +0100

    Rubrica 21
    Signed-off-by: Jaime <jaime.viartola@gmail.com>

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -27,6 +27,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
